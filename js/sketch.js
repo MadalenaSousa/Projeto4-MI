2b7be4f2115a653b2bf6e909ff692a865e175065
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-02-25 12:51:34 +0000

    added p5

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  // put setup code here
+    createCanvas(1920, 1080);
+}
+
+function draw() {
+  // put drawing code here
+    stroke(0);
+    ellipse(mouseX, mouseY, 100, 100);
+}
