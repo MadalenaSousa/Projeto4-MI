2fc644b340f081eb4a0abefeae419719ea40870e
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-02-25 12:38:05 +0000

    testes

--- getTracks.php
+++ getTracks.php
@@ -8,3 +8,17 @@ function getTracks($api) {
         echo '<b>' . $track->name . '</b> <br>';
     }
 }
+
+function getTrackAudioFeatures($api){
+    $analysis = $api->getAudioFeatures('2p2aCtErYX6UaFxyfpVvWC');
+
+    return $analysis;
+}
+
+function listUserPlaylists($api, $userId) {
+    $playlists = $api->getUserPlaylists($userId);
+
+    foreach ($playlists->items as $playlist) {
+        echo '<a href="' . $playlist->external_urls->spotify . '">' . htmlspecialchars($playlist->name) . '</a> <br>';
+    }
+}
--- loginSpotify.php
+++ loginSpotify.php
@@ -2,11 +2,10 @@
 require 'vendor/autoload.php';
 include_once "getTracks.php";
 
-
 $session = new SpotifyWebAPI\Session(
     'de8b0fa92d7a421da94cd48858522a77',
     'a713fdaaef5347ae801749567d4e82e8',
-    'http://localhost/P4/loginSpotify.php'
+    'http://localhost/Projeto4-MI/loginSpotify.php'
 );
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
@@ -15,13 +14,29 @@ if (isset($_GET['code'])) {
     $session->requestAccessToken($_GET['code']);
     $api->setAccessToken($session->getAccessToken());
 
-    getTracks($api);
+    //getTracks($api);
 
-    print_r($api->me());
+    $features = getTrackAudioFeatures($api)->{'audio_features'}[0];
+    print_r($features);
+
+    echo '<br><br>' . $features->{'energy'} . '<br><br>';
+
+    $meusDados = $api->me();
+    print_r($meusDados);
+
+    echo '<br><br>';
+
+    listUserPlaylists($api, $meusDados->{'id'});
 } else {
     $options = [
         'scope' => [
             'user-read-email',
+            'user-read-private',
+            'user-read-playback-state',
+            'user-top-read',
+            'user-read-recently-played',
+
+            'playlist-read-private',
         ],
     ];
 
