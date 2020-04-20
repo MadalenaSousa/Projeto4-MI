712cca65d842d0cca6bf1dee128ec9c3d4dfc1ad
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-04-20 15:06:41 +0100

    uhuh

--- js/playlists.js
+++ js/playlists.js
@@ -35,7 +35,15 @@ function setup() {
                 px: random(0, windowWidth-200),
                 py: random(0, windowHeight-200),
                 color: color(255),
-                largura: userPlaylists[i].tracks.total
+                largura: userPlaylists[i].tracks.total,
+                resolution: 13, // número de "vértices"
+                rad: 90,  //tamanho
+                round: random(0, 10), //valor q ainda nao percebi o q afeta bem
+                nInt: 500, // tmb n sei q faz
+                nAmp: 0.4, // valor=1 -> redonda
+                nSeed: random(0, 60), // tambem n sei q afeta
+                t: 0,
+                tChange: 0.01
             });
 
             recordList.addEntry(userPlaylists[i].name);
@@ -49,14 +57,16 @@ function setup() {
 
             currentRecord.whenReady(function () {
                 console.log(recordList.getEntries());
-                addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('largura'), currentRecord.get('color'));
+                addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('largura'), currentRecord.get('color'),
+                    currentRecord.get('resolution'), currentRecord.get('rad'), currentRecord.get('round'), currentRecord.get('nInt'), currentRecord.get('nAmp'),
+                    currentRecord.get('nSeed'), currentRecord.get('t'), currentRecord.get('tChange'));
             });
         }
     }, true);
 }
 
-function addNewFlower(name, px, py, largura, color) {
-    newMountain = new classMountain(name, px, py, largura, color);
+function addNewFlower(name, px, py, largura, color, resolution, rad, round, nInt, nAmp, nSeed, t, tChange) {
+    newMountain = new classMountain(name, px, py, largura, color, resolution, rad, round, nInt, nAmp, nSeed, t, tChange);
     mountains.push(newMountain);
     console.log(mountains);
 }
@@ -74,27 +84,28 @@ function draw() {
 
 class classMountain {
     c;
-    /*
-    nval;
-    resolution= 13; // número de "vértices"
-    rad= 200;  //tamanho
-    round= random(0, 100); //valor q ainda nao percebi o q afeta bem
-    nInt= 500; // tmb n sei q faz
-    nAmp= 0.4; // valor=1 -> redonda
-    nSeed= random(0, 60); // tambem n sei q afeta
-    t = 0;
-    tChange = 0.01;*/
+    nVal;
+    x;
+    y;
 
-    constructor(name, px, py, largura, color) {
+    constructor(name, px, py, largura, color, resolution, rad, round, nInt, nAmp, nSeed, t, tChange) {
         this.name = name;
         this.px = px;
         this.py = py;
         this.largura = largura;
-        this.color  = color;
+        this.resolution  = resolution;
+        this.rad  = rad;
+        this.round  = round;
+        this.nInt  = nInt;
+        this.nAmp  = nAmp;
+        this.nSeed  = nSeed;
+        this.t  = t;
+        this.tChange  = tChange;
+
     }
 
     display() {
-        if(dist(mouseX, mouseY, this.px, this.py) <= this.largura){
+        if(dist(mouseX, mouseY, this.px, this.py) <= this.largura*5){
             this.c = color(0,200,255);
 
         } else {
@@ -109,21 +120,21 @@ class classMountain {
         rect(this.px, this.py, this.largura * 2, this.largura * 2);
 
         //****************************************************************************************
-        /*beginShape();
-        for (let a=-1; a <= TWO_PI; a += TWO_PI/resolution) {
+        beginShape();
+        for (let a=- 1; a <= TWO_PI; a += TWO_PI/this.resolution) {
+            this.nVal = map(noise(cos(a)*this.nInt+this.nSeed, sin(a)*this.nInt+this.nSeed,this.t), 0.0, 1.0, this.nAmp, 1.0);
 
-            nVal = map(noise(cos(a)*nInt+nSeed, sin(a)*nInt+nSeed, t), 0.0, 1.0, nAmp, 1.0);
+            this.x = this.px + cos(a)*(this.rad+this.round) *this.nVal;
+            this.y = this.py + sin(a)*this.rad *this.nVal;
 
-            x = px + cos(a)*(rad+round) *nVal;
-            y = py + sin(a)*rad *nVal;
+            curveVertex(this.x, this.y);
 
-            curveVertex(x, y);
         }
+
         endShape(CLOSE);
 
-        //t += tChange;
+        this.t += this.tChange;
         //****************************************************************************************
-*/
         //nome da playlist
         noStroke();
         fill(this.c);
