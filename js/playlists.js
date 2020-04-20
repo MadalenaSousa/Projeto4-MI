db85456f69680975954bf6e362ca953875fe4cec
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-04-20 14:43:46 +0100

    update_funciona agr

--- js/playlists.js
+++ js/playlists.js
@@ -7,6 +7,7 @@ const client = new DeepstreamClient('localhost:6020');
 const record = [];
 let recordList;
 
+
 function preload() {
     userPlaylists = loadJSON('php/playlist-object.json');
 }
@@ -31,8 +32,8 @@ function setup() {
             record[i] = client.record.getRecord(userPlaylists[i].name); //crio um novo record no servidor
             record[i].set({ //defino o novo record
                 playlist: userPlaylists[i].name,
-                x: random(0, windowWidth),
-                y: random(0, windowHeight),
+                px: random(0, windowWidth-200),
+                py: random(0, windowHeight-200),
                 color: color(255),
                 largura: userPlaylists[i].tracks.total
             });
@@ -48,14 +49,14 @@ function setup() {
 
             currentRecord.whenReady(function () {
                 console.log(recordList.getEntries());
-                addNewFlower(currentRecord.get('playlist'), currentRecord.get('x'), currentRecord.get('y'), currentRecord.get('largura'), currentRecord.get('color'));
+                addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('largura'), currentRecord.get('color'));
             });
         }
     }, true);
 }
 
-function addNewFlower(name, x, y, largura, color) {
-    newMountain = new classMountain(name, x, y, largura, color);
+function addNewFlower(name, px, py, largura, color) {
+    newMountain = new classMountain(name, px, py, largura, color);
     mountains.push(newMountain);
     console.log(mountains);
 }
@@ -73,33 +74,61 @@ function draw() {
 
 class classMountain {
     c;
+    /*
+    nval;
+    resolution= 13; // número de "vértices"
+    rad= 200;  //tamanho
+    round= random(0, 100); //valor q ainda nao percebi o q afeta bem
+    nInt= 500; // tmb n sei q faz
+    nAmp= 0.4; // valor=1 -> redonda
+    nSeed= random(0, 60); // tambem n sei q afeta
+    t = 0;
+    tChange = 0.01;*/
 
-    constructor(name, x, y, largura, color) {
+    constructor(name, px, py, largura, color) {
         this.name = name;
-        this.x = x;
-        this.y = y;
+        this.px = px;
+        this.py = py;
         this.largura = largura;
         this.color  = color;
     }
 
     display() {
-        if(dist(mouseX, mouseY, this.x, this.y) <= this.largura){
+        if(dist(mouseX, mouseY, this.px, this.py) <= this.largura){
             this.c = color(0,200,255);
 
         } else {
             this.c = color(255);
         }
 
+        //desenho
         stroke(this.c);
         strokeWeight(2);
         noFill();
         rectMode(CENTER);
-        rect(this.x, this.y, this.largura * 2, this.largura * 2);
+        rect(this.px, this.py, this.largura * 2, this.largura * 2);
 
+        //****************************************************************************************
+        /*beginShape();
+        for (let a=-1; a <= TWO_PI; a += TWO_PI/resolution) {
+
+            nVal = map(noise(cos(a)*nInt+nSeed, sin(a)*nInt+nSeed, t), 0.0, 1.0, nAmp, 1.0);
+
+            x = px + cos(a)*(rad+round) *nVal;
+            y = py + sin(a)*rad *nVal;
+
+            curveVertex(x, y);
+        }
+        endShape(CLOSE);
+
+        //t += tChange;
+        //****************************************************************************************
+*/
+        //nome da playlist
         noStroke();
         fill(this.c);
         textSize(12);
-        text(this.name, this.x, this.y);
+        text(this.name, this.px, this.py);
     }
 
 }
