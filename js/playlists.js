3b3e072aca966c4081e49cda4810f89f3d575ab9
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-04-20 17:38:56 +0100

    e uma tarde se vai

--- js/playlists.js
+++ js/playlists.js
@@ -32,16 +32,18 @@ function setup() {
             record[i] = client.record.getRecord(userPlaylists[i].name); //cria um novo record no servidor
             record[i].set({ //define o novo record
                 playlist: userPlaylists[i].name,
-                px: random(0, windowWidth-300),
-                py: random(0, windowHeight-300),
+                px: random(150, windowWidth-550),
+                py: random(150, windowHeight-100),
                 color: color(255),
                 largura: userPlaylists[i].tracks.total,
-                resolution: map(userPlaylists[i].tracks.total, 0.0, 500, 30, 100), // número de "vértices"
-                rad: 70,  //tamanho
-                round: map(userPlaylists[i].average_features.energy, 0.0, 1.0, 90, 0), //quanto maior o valor, mais espalmada
-                nAmp: map(userPlaylists[i].average_features.loudness, -60, 0, 0.1, 1), // valor=1 -> redonda
+                resolution: map(userPlaylists[i].tracks.total, 0, 500, 13, 20), // número de "vértices"
+                tam: map(userPlaylists[i].average_features.positivity, 0, 1.0, 10, 70),  //tamanho
+                round: map(userPlaylists[i].average_features.energy, 0.0, 1.0, 30, 0), //quanto maior o valor, mais espalmada
+                nAmp: map(userPlaylists[i].average_features.loudness, -60, 0, 0.3, 1), // valor=1 -> redonda
                 t: 0,
-                tChange: map(userPlaylists[i].average_features.danceability, 0.0, 1.0, 0.01, 0.2) // dança do objeto
+                tChange: map(userPlaylists[i].average_features.danceability, 0.0, 1.0, 0.01, 0.06), // dança do objeto
+                nInt: 10, //intensidade
+                nSeed: 10
             });
 
             recordList.addEntry(userPlaylists[i].name);
@@ -56,15 +58,15 @@ function setup() {
             currentRecord.whenReady(function () {
                 console.log(recordList.getEntries());
                 addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('largura'), currentRecord.get('color'),
-                    currentRecord.get('resolution'), currentRecord.get('rad'), currentRecord.get('round'), currentRecord.get('nAmp'),
-                    currentRecord.get('t'), currentRecord.get('tChange'));
+                    currentRecord.get('resolution'), currentRecord.get('tam'), currentRecord.get('round'), currentRecord.get('nAmp'),
+                    currentRecord.get('t'), currentRecord.get('tChange'), currentRecord.get('nInt'), currentRecord.get('nSeed'));
             });
         }
     }, true);
 }
 
-function addNewFlower(name, px, py, largura, color, resolution, rad, round, nAmp, t, tChange) {
-    newMountain = new classMountain(name, px, py, largura, color, resolution, rad, round, nAmp, t, tChange);
+function addNewFlower(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
+    newMountain = new classMountain(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed);
     mountains.push(newMountain);
     console.log(mountains);
 }
@@ -85,18 +87,19 @@ class classMountain {
     x;
     y;
 
-    constructor(name, px, py, largura, color, resolution, rad, round, nAmp, t, tChange) {
+    constructor(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
         this.name = name;
         this.px = px;
         this.py = py;
         this.largura = largura;
         this.resolution  = resolution;
-        this.rad  = rad;
+        this.tam  = tam;
         this.round  = round;
         this.nAmp  = nAmp;
         this.t  = t;
         this.tChange  = tChange;
-
+        this.nInt=nInt;
+        this.nSeed=nSeed;
     }
 
     display() {
@@ -115,11 +118,11 @@ class classMountain {
 
         beginShape();
         for (let b=1; b<=2; b++) {
-            for (let a = -1; a <= TWO_PI; a += TWO_PI / this.resolution) {
-                this.nVal = map(noise(cos(a), sin(a), this.t), 0.0, 1.0, this.nAmp, 1.0);
+            for (let a = -1; a <= 5; a += 5/ this.resolution) {
+                this.nVal = map(noise(cos(a)*this.nInt+this.nSeed, sin(a)*this.nInt+this.nSeed, this.t), 0.0, 1.0, this.nAmp, 2.0);
 
-                this.x = this.px + cos(a) * (this.rad + this.round) * this.nVal;
-                this.y = this.py + sin(a) * this.rad * this.nVal;
+                this.x = this.px + cos(a) * (this.tam + this.round) * this.nVal;
+                this.y = this.py + sin(a) * this.tam * this.nVal;
 
                 curveVertex(this.x, this.y);
 
