7aad69d192c35c5a4bce6744ebe2ecbf27e7962b
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-04-20 19:20:32 +0100

    n desenha c menos 10

--- js/playlists.js
+++ js/playlists.js
@@ -35,9 +35,9 @@ function setup() {
                 px: random(150, windowWidth-550),
                 py: random(150, windowHeight-100),
                 color: color(255),
-                largura: userPlaylists[i].tracks.total,
-                resolution: map(userPlaylists[i].tracks.total, 0, 500, 13, 20), // número de "vértices"
-                tam: map(userPlaylists[i].average_features.positivity, 0, 1.0, 10, 70),  //tamanho
+                numtracks: userPlaylists[i].tracks.total,
+                resolution: map(userPlaylists[i].average_features.positivity, 0, 1.0, 13, 20),// número de "vértices"
+                tam: map(userPlaylists[i].tracks.total, 0, 500, 0, 70), //tamanho
                 round: map(userPlaylists[i].average_features.energy, 0.0, 1.0, 30, 0), //quanto maior o valor, mais espalmada
                 nAmp: map(userPlaylists[i].average_features.loudness, -60, 0, 0.3, 1), // valor=1 -> redonda
                 t: 0,
@@ -57,7 +57,7 @@ function setup() {
 
             currentRecord.whenReady(function () {
                 console.log(recordList.getEntries());
-                addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('largura'), currentRecord.get('color'),
+                addNewFlower(currentRecord.get('playlist'), currentRecord.get('px'), currentRecord.get('py'), currentRecord.get('numtracks'), currentRecord.get('color'),
                     currentRecord.get('resolution'), currentRecord.get('tam'), currentRecord.get('round'), currentRecord.get('nAmp'),
                     currentRecord.get('t'), currentRecord.get('tChange'), currentRecord.get('nInt'), currentRecord.get('nSeed'));
             });
@@ -65,8 +65,8 @@ function setup() {
     }, true);
 }
 
-function addNewFlower(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
-    newMountain = new classMountain(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed);
+function addNewFlower(name, px, py, numtracks, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
+    newMountain = new classMountain(name, px, py, numtracks, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed);
     mountains.push(newMountain);
     console.log(mountains);
 }
@@ -87,11 +87,11 @@ class classMountain {
     x;
     y;
 
-    constructor(name, px, py, largura, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
+    constructor(name, px, py, numtracks, color, resolution, tam, round, nAmp, t, tChange, nInt, nSeed) {
         this.name = name;
         this.px = px;
         this.py = py;
-        this.largura = largura;
+        this.numtracks = numtracks;
         this.resolution  = resolution;
         this.tam  = tam;
         this.round  = round;
@@ -103,7 +103,7 @@ class classMountain {
     }
 
     display() {
-        if(dist(mouseX, mouseY, this.px, this.py) <= this.largura*5){
+        if(dist(mouseX, mouseY, this.px, this.py) <= this.numtracks*5){
             this.c = color(0,200,255);
             this.t += this.tChange;
             //nome da playlist
@@ -121,19 +121,18 @@ class classMountain {
         strokeWeight(1.5);
         noFill();
 
-        beginShape();
-        for (let b=1; b<=2; b++) {
+
+        for (let b=1; b<=(this.numtracks)/10; b++) {
+            beginShape();
             for (let a = -1; a <= 5; a += 5/ this.resolution) {
                 this.nVal = map(noise(cos(a)*this.nInt+this.nSeed, sin(a)*this.nInt+this.nSeed, this.t), 0.0, 1.0, this.nAmp, 2.0);
 
-                this.x = this.px + cos(a) * (this.tam + this.round) * this.nVal;
-                this.y = this.py + sin(a) * this.tam * this.nVal;
+                this.x = this.px + (cos(a) * (this.tam + this.round) * this.nVal)/b;
+                this.y = this.py + (sin(a) * (this.tam + this.round) * this.nVal)/b;
 
                 curveVertex(this.x, this.y);
-
             }
+            endShape(CLOSE);
         }
-        endShape(CLOSE);
     }
-
 }
