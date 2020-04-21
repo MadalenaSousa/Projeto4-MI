a42cb55e0410a5f2f5cc544c6dd177cf6c2d7301
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-04-21 11:22:57 +0100

    Update tracks.js

--- js/tracks.js
+++ js/tracks.js
@@ -18,7 +18,8 @@ function preload() {
 function setup() {
     createCanvas(windowWidth - windowWidth/6, windowHeight);
     client.login({username: user.name});
-    songs = topSongs;
+    user.delete();}
+    /*songs = topSongs;
 
     totalSongs = Object.keys(songs).length;
 
@@ -220,4 +221,4 @@ class flowerSong {
             this.musicOn = !this.musicOn;
         }
     }
-}
+}*/
