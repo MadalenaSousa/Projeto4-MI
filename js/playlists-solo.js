3aca57a38a616f0b442b8ee3ab7bc92f27c9aadc
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-05-13 22:53:55 +0100

    isto

--- js/playlists-solo.js
+++ js/playlists-solo.js
@@ -5,9 +5,13 @@ let remove;
 
 const client = new DeepstreamClient('localhost:6020');
 const record = [];
+let personRecord;
+let clientsRecords = [];
 let recordList;
 
 let trackstotal=[];
+let speedX=[];
+let loudnessY=[];
 
 
 function preload() {
@@ -17,17 +21,66 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth - windowWidth/6, windowHeight);
+    client.login({username: user.name}, (success, data) => {
+        if(success) {
+            console.log("User logged in successfully");
+            client.record.has(user.name, function (error, hasRecord) {
+                console.log(error);
+                if(hasRecord === false) {
+                    console.log("Record of this user doesnt exist, it will be created");
+                    personRecord = client.record.getRecord(user.name);
+                    personRecord.set({
+                        name: user.name,
+                        id: user.id,
+                        profile_pic: user.profile_pic
+                    });
+                } else {
+                    console.log("A record of this user already exists, it will be retrieved");
+                    personRecord = client.record.getRecord(user.name);
+                }
+            });
+        } else {
+            console.log('Login failed');
+        }
+    });
 
-    client.login();
+    client.presence.getAll((error, clients) => {
+        for(let i = 0; i < clients.length; i++){
+            console.log('Clients present on login: ' + clients);
+            clientsRecords[i] = client.record.getRecord(clients[i]);
+            clientsRecords[i].subscribe(function () {
+                createUserDiv(clientsRecords[i].get('name'), clientsRecords[i].get('profile_pic'))
+            });
+        }
+    });
+
+    client.presence.subscribe((username, isLoggedIn) => {
+        if(isLoggedIn){
+            console.log('A new client logged in');
+            clearArray(clientsRecords);
+            client.presence.getAll((error, clients) => {
+                for(let i = 0; i < clients.length; i++){
+                    console.log('Updated clients list: ' + clients);
+                    clientsRecords[i] = client.record.getRecord(clients[i]);
+                    clientsRecords[i].subscribe(function () {
+                        createUserDiv(clientsRecords[i].get('name'), clientsRecords[i].get('profile_pic'))
+                    });
+                }
+            });
+        }
+    });
+
     totalPlaylists = Object.keys(userPlaylists).length;
 
-    createUserDiv();
+    createUserDiv(user.name, user.profile_pic);
     createPlaylistDiv();
     logoutPopUp();
     sharePopUp();
 
     for(let i = 0; i < totalPlaylists; i++) {
         trackstotal.push(userPlaylists[i].tracks.total);
+        speedX.push(userPlaylists[i].average_features.speed);
+        loudnessY.push(userPlaylists[i].average_features.loudness);
     }
 
     recordList = client.record.getList('all-playlists');
@@ -68,9 +121,10 @@ function setup() {
                     console.log('doesnt have record with name: ' + userPlaylists[i].name + ", can create it");
                     record[i] = client.record.getRecord(userPlaylists[i].name); //cria um novo record no servidor
                     record[i].set({ //define o novo record
+                        user: user.name,
                         playlist: userPlaylists[i].name,
-                        px: random(100, windowWidth-400),
-                        py: random(100, windowHeight-100),
+                        px: map(userPlaylists[i].average_features.speed, min(speedX), max(speedX), 110, windowWidth - 375),
+                        py: map(userPlaylists[i].average_features.loudness, min(loudnessY), max(loudnessY), 150, windowHeight - 110),
                         color: color(255),
                         numtracks: userPlaylists[i].tracks.total,
                         resolution: map(userPlaylists[i].average_features.positivity, 0, 1.0, 13, 20),// número de "vértices"
@@ -109,6 +163,8 @@ function setup() {
         });
 
     }
+
+    document.querySelector('.confirm-logout').addEventListener('click', closePlaylistRoomConnection);
 
     document.querySelector('.download').addEventListener('click', function () {
         console.log('Canvas will be downloaded');
@@ -183,16 +239,16 @@ function createPlaylistDiv() {
     }
 }
 
-function createUserDiv() {
+function createUserDiv(name, profilepic) {
     let userDiv = document.createElement('div');
     let person = document.createElement('div');
     let img = document.createElement('img');
 
-    img.setAttribute('src', user.profile_pic);
+    img.setAttribute('src', profilepic);
     img.setAttribute('width', '30px');
     img.setAttribute('height', '30px');
 
-    person.innerText = user.name;
+    person.innerText = name;
     person.classList.add('username');
 
     userDiv.classList.add('user');
@@ -200,6 +256,41 @@ function createUserDiv() {
     userDiv.appendChild(person);
 
     document.querySelector(".list-people").appendChild(userDiv);
+}
+
+function closePlaylistRoomConnection() {
+    let allRecords = [];
+    let recordsToRemove = [];
+
+    for(let i = 0; i < recordList.getEntries().length; i++) {
+        allRecords[i] = client.record.getRecord(recordList.getEntries()[i]);
+        allRecords[i].whenReady(function () {
+            console.log('Record to delete: ' + allRecords[i].get('playlist') + ' Owner of the record: ' + allRecords[i].get('user'));
+            if (allRecords[i].get('user') === user.name) {
+                recordsToRemove.push(allRecords[i]);
+            }
+        });
+    }
+
+    if(recordsToRemove.length === 0) {
+        client.close();
+    } else {
+        for(let i = 0; i < recordsToRemove.length; i++) {
+            recordList.removeEntry(recordsToRemove[i].get('playlist'));
+            client.record.getRecord(recordsToRemove[i].get('playlist')).delete();
+        }
+
+        recordsToRemove[recordsToRemove.length - 1].on('delete', function () {
+            client.close();
+        });
+    }
+
+    client.on('connectionStateChanged', connectionState => {
+        if(connectionState === 'CLOSED') {
+            console.log('Connection state changed to: ' + connectionState + ', you will be redirected to homepage');
+            document.location = './homepage.php';
+        }
+    });
 }
 
 function draw() {
@@ -239,12 +330,6 @@ class classMountain {
             this.c = color(0, 200, 255);
             this.t += this.tChange;
 
-            //nome da playlist
-            noStroke();
-            fill(this.c);
-            textSize(12);
-            text(this.name, this.px, this.py);
-
         }
         else this.c = color(255);
 
@@ -260,17 +345,29 @@ class classMountain {
 
 
         if (this.numtracks <= 30) {
-            this.tam = map(this.numtracks, 0, 20, 20, 40);
+            this.tam = map(this.numtracks, 0, 30, 10, 30);
         }
         else if((this.numtracks > 30) && (this.numtracks <= 60)) {
-            this.tam = map(this.numtracks, 31, 60, 40, 60);
+            this.tam = map(this.numtracks, 31, 60, 30, 50);
         }
-        else if((this.numtracks > 60) && (this.numtracks <= 100)) {
-            this.tam = map(this.numtracks, 61, 100, 60, 80);
+        else if((this.numtracks > 60) && (this.numtracks <= 90)) {
+            this.tam = map(this.numtracks, 61, 90, 50, 60);
         }
-        else if(this.numtracks > 100) this.tam = 95;
+        else if((this.numtracks > 90) && (this.numtracks <= 120)) {
+            this.tam = map(this.numtracks, 91, 120, 60, 70);
+        }
+        else if((this.numtracks > 120) && (this.numtracks <= 200)) {
+            this.tam = map(this.numtracks, 121, 200, 70, 80);
+        }
+        else if((this.numtracks > 200) && (this.numtracks <= 400)) {
+            this.tam = map(this.numtracks, 201, 400, 80, 90);
+        }
+        else if((this.numtracks > 400) && (this.numtracks <= 600)) {
+            this.tam = map(this.numtracks, 401, 600, 90, 100);
+        }
+        else if(this.numtracks > 600) this.tam = 105;
 
-
+        //fill(0);
         for (let b = 1; b <= (this.tam) / 10; b++) {
             beginShape();
             for (let a = -1; a <= 5; a += 5 / this.resolution) {
@@ -283,7 +380,14 @@ class classMountain {
             }
             endShape(CLOSE);
         }
+        //nome da playlist
+        textStyle(BOLD);
+        noStroke();
+        fill(this.c);
+        textSize(12);
+        text(this.name, this.px, this.py);
     }
+
     balao(){
         //caixa de informação
         fill(0);
