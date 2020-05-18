0d7b3f249fc9b4d042e7e55d099776ffe9d4dc1c
Madalena Sousa <madalenaantunessousa@gmail.com>
2020-05-18 18:47:58 +0100

    partilhas

--- tracks.php
+++ tracks.php
@@ -74,20 +74,30 @@ $session_value = (isset($_SESSION['userData'])) ? $_SESSION['userData'] : '';
         You can share the artboard you created in your social networks or download it and use it for anything you want!
     </div>
     <div class="unit">
-        <div class="menu-option">
-            <div><img src="icons/facebook.png"></div>
-            <div>Facebook</div>
-        </div>
+
+        <a target="popup"
+           onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=http://music-visualizer.duckdns.org:8888/','popup','width=600,height=400');">
+            <div class="menu-option">
+                <img src="icons/facebook.png">
+                <div>Facebook</div>
+            </div>
+        </a>
+
         <!--div class="menu-option">
             <div><img src="icons/instagram.png"></div>
             <div>Instagram</div>
         </div-->
-        <div class="menu-option">
-            <div><img src="icons/twitter.png"></div>
-            <div>Twitter</div>
-        </div>
+
+        <a target="popup"
+           onclick="window.open('https://twitter.com/intent/tweet?text=Look%20at%20my%20sea%20of%20my%20Top%20Artists%20on%20Spotify','popup','width=600,height=400');">
+            <div class="menu-option">
+                <img src="icons/twitter.png">
+                <div>Twitter</div>
+            </div>
+        </a>
+
         <div class="menu-option download">
-            <div><img src="icons/download.png"></div>
+            <img src="icons/download.png">
             <div>Download</div>
         </div>
     </div>
