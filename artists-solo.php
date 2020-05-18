2f213434a1b2d53c8286a682008355c3dd19f204
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2020-05-18 20:54:37 +0100

    Update artists-solo.php

--- artists-solo.php
+++ artists-solo.php
@@ -32,7 +32,7 @@ $session_value = (isset($_SESSION['userData'])) ? $_SESSION['userData'] : '';
         <div class="leave">Leave Room</div>
     </div>
     <div>
-        People in the Room
+        You are the only person in the room
         <div class="list-people unit">
         </div>
         <div>
