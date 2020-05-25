46eca74bbaa1932ed2550effd250b22334f7b08e
Maria Figueira <34713532+mariafigueira@users.noreply.github.com>
2020-05-25 16:38:12 +0100

    Update artists.js

--- js/artists.js
+++ js/artists.js
@@ -122,18 +122,18 @@ function setup() {
                     let speedX = 0;
                     let speedValorX = 0;
                     if (recordsOnList[i].get('x') > max(speed)) {
-                        speedX = width - 375;
+                        speedX = width - 150;
                     } else if (recordsOnList[i].get('x') < min(speed)) {
                         speedX = width / (width / 300);
                     } else {
-                        speedX = map(recordsOnList[i].get('x'), min(speed), max(speed), width / (width / 300), width - 375);
+                        speedX = map(recordsOnList[i].get('x'), min(speed), max(speed), width / (width / 300), width - 150);
                     }
                     if (recordsOnList[i].get('valorX') > max(speed)) {
-                        speedValorX = width - 375;
+                        speedValorX = width - 150;
                     } else if (recordsOnList[i].get('valorX') < min(speed)) {
                         speedValorX = width / (width / 300);
                     } else {
-                        speedValorX = map(recordsOnList[i].get('valorX'), min(speed), max(speed), width / (width / 300), width - 375);
+                        speedValorX = map(recordsOnList[i].get('valorX'), min(speed), max(speed), width / (width / 300), width - 150);
                     }
                     addNewWave(recordsOnList[i].get('artist'), recordsOnList[i].get('color'), recordsOnList[i].get('divisoes'), recordsOnList[i].get('largura'), speedX, recordsOnList[i].get('y'), recordsOnList[i].get('shake'), speedValorX, recordsOnList[i].get('valorY'), recordsOnList[i].get('id'), recordsOnList[i].get('user'));
                 });
@@ -530,7 +530,7 @@ class waveArtist {
                 text("Danceability: " + map(this.divisoes, 3, 10, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * (this.largura / 2)) / 3) - 130);
                 text("Positivity: " + map(this.largura, 100, 400, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * (this.largura / 2)) / 3) - 110);
                 text("Loudness: " + map(this.valorY, 250, height - 50, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * (this.largura / 2)) / 3) - 90);
-                text("Speed: " + map(this.valorX, 125, width - (width / 26) - 375, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * (this.largura / 2)) / 3) - 70);
+                text("Speed: " + map(this.valorX, 125, width - 150, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * (this.largura / 2)) / 3) - 70);
 
 
                 if (mouseX > this.x + 10 && mouseX < this.x + 10 + 110 && mouseY > (this.y - ((2 * (this.largura / 2)) / 3)) - 50 && mouseY < (this.y - ((2 * (this.largura / 2)) / 3)) - 50 + 20) {
@@ -576,7 +576,7 @@ class waveArtist {
                 text("Danceability: " + map(this.divisoes, 3, 10, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * this.largura / 2) / 3) + 130);
                 text("Positivity: " + map(this.largura, 100, 400, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * this.largura / 2) / 3) + 110);
                 text("Loudness: " + map(this.valorY, 250, height - 50, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * this.largura / 2) / 3) + 90);
-                text("Speed: " + map(this.valorX, 125, width - (width / 26) - 375, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * this.largura / 2) / 3) + 70);
+                text("Speed: " + map(this.valorX, 125, width- 150, 0, 100).toFixed(1) + "%", this.x + 10, this.y - ((2 * this.largura / 2) / 3) + 70);
 
 
                 if (mouseX > this.x + 10 && mouseX < this.x + 10 + 110 && mouseY > (this.y - ((2 * this.largura / 2) / 3) + 150) && mouseY < (this.y - ((2 * this.largura / 2) / 3) + 150 + 20)) {
