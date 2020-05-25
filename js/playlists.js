6a801fdae51f2314cb2ec4279ae0f1b3b9786a85
mariaspcc <43615324+mariaspcc@users.noreply.github.com>
2020-05-25 16:56:31 +0100

    done

--- js/playlists.js
+++ js/playlists.js
@@ -518,8 +518,8 @@ class classMountain {
         text("Energy: " + map(this.round, 30,0, 0.0, 1.0).toFixed(1)*100 + "%", this.px + 10, this.py - 190 + (this.valor*7.7));
         text("Danceability: " + map(this.tChange, 0.01, 0.06, 0.0, 1.0).toFixed(1)*100 + "%", this.px + 10, this.py - 170 + (this.valor*7.7));
         text("Positivity: " + map(this.resolution, 13, 20, 0, 1.0).toFixed(1)*100 + "%", this.px + 10, this.py - 150 + (this.valor*7.7));
-        text("Loudness: " + map(this.py, 0.3, 1, 0, 100).toFixed(1) + "%", this.px + 10, this.py - 130 + (this.valor*7.7));
-        text("Speed: " + map(this.px, 13, 20, 0, 1.0).toFixed(1)*100 + "%", this.px + 10, this.py - 110 + (this.valor*7.7));
+        text("Loudness: " + map(this.py, height,0, 0, 100).toFixed(1) + "%", this.px + 10, this.py - 130 + (this.valor*7.7));
+        text("Speed: " + map(this.px, 0, width, 0, 100).toFixed(1) + "%", this.px + 10, this.py - 110 + (this.valor*7.7));
         text("Total songs: " + this.numtracks, this.px + 10, this.py - 90 + (this.valor*7.7));
 
         if((mouseX > this.px + 10) && (mouseX < this.px + 110) && (mouseY > this.py - 80 + (this.valor*3.5)) && (mouseY < this.py - 60)+ (this.valor*4)) {
